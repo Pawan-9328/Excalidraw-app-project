56bc6b4a71d99b4f3b16dc9289d94628bc43ff43
pawaan <pawaan@pawaans-MacBook-Air.local>
2025-12-30 12:28:23 +0530

    Step 9 - Initialized http/ws server

---
+++ apps/http-backend/src/index.ts
@@ -0,0 +1,12 @@
+
+import express from "express"; 
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`HTTP backend running on port ${PORT}`);
+});
+
+
