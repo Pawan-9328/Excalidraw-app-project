c9a05f528709991000633b4f8004d28dc3e6eb79
pawaan <pawaan@pawaans-MacBook-Air.local>
2025-12-30 16:02:46 +0530

    Added steps 10, 11, 12

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,7 +1,44 @@
-
-import express from "express"; 
+import express from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
 
 const app = express();
+
+
+app.post("/signup", (req, res) => {
+  //db call 
+   res.json({
+     userId: "123"
+   })
+
+
+})
+
+app.post("/signin", (req, res) => {
+
+
+  const userId = 1;
+  const token = jwt.sign({
+    userId
+  }, JWT_SECRET);
+
+  res.json({
+    token
+  })
+})
+
+app.post("/room", (req, res) => {
+  //db call 
+  res.json({
+    roomId: 123
+  })
+
+
+})
+
+
+
+
 
 const PORT = process.env.PORT || 4000;
 
